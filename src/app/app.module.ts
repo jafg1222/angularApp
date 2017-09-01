d8eeedf845308991ed72711bc2c66d735fcb52d7
Jose Flores Garcia <jose_01cr@outlook.com>
2017-09-01 11:27:42 -0600

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BookmarkComponent } from './modelo/bookmark/bookmark.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BookmarkComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modelo/bookmark.ts
@@ -0,0 +1,13 @@
+export class Bookmark {
+    
+    public titulo:string;
+    public nota:string;
+    public fecha:string;
+
+    constructor(titulo:string, nota:string, fecha:string ){
+        this.titulo = titulo;
+        this.nota = nota;
+        this.fecha = fecha;
+    };
+
+}
